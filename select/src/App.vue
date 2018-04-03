054cf8fc0736538e5ebc700371120882d7499f6d
15801074413 <3329925722@qq.com>
2018-04-03 21:06:09 +0800

    李帅奇

---
+++ select/src/App.vue
@@ -0,0 +1,164 @@
+<template>
+  <div id="app">
+    <div class="header clear">
+      <div class="logo">
+          <img src="../public/img/logo.png" alt="">
+      </div>
+      <div id="nav">
+          <div>
+            <router-link to="/">首页</router-link>
+            <router-link to="/about/1">全新楼盘</router-link>
+            <router-link to="/about/2">二手楼盘</router-link>
+            <router-link to="/about/3">全新商铺</router-link>
+            <router-link to="">楼市动态</router-link>
+            <router-link to="">联系我们</router-link>
+          </div>
+      </div>
+      <div class="tal">
+        <div>
+          <img src="../public/img/tal.png" alt="">
+          <span>	
+              008-6666 6666</span>
+        </div>
+        <div>咨询电话</div>
+      </div>
+    </div>
+    <router-view/>
+    <div class="footer">
+      <span>地产REALSTATE  © 2020 All Rights Reserved.</span>
+      <div>
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  font-size: 13px;
+  font-family: "Open Sans",Helvetica,Arial,Verdana,sans-serif;
+  box-sizing: inherit;
+}
+
+.clear:after{
+  display:block;
+  clear:both;
+  content:"";
+  visibility:hidden;
+  height:0
+} 
+
+.clear{
+  zoom:1
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #000000;
+  position: relative;
+  min-height: 182px;
+}
+
+a{
+  color: #000000;
+}
+
+#nav {
+  float: left;
+  width: 737px;
+  height: 100px;
+}
+
+#nav div{
+  width: 552px;
+  margin: 20px 0;
+  line-height: 60px;
+  position: relative;
+  left: 83px;
+}
+
+#nav div a{
+  display: inline-block;
+  line-height: 60px;
+  margin: 0 20px;
+}
+
+#nav div a:first-child{
+  margin-left: 0;
+}
+
+#nav div a:last-child{
+  margin-right: 0;
+}
+
+.header {
+  max-width: 1190px;
+  margin: 0 auto;
+}
+
+.header .logo{
+  float: left;
+  width: 209px;
+  height: 80px;
+  line-height: 80px;
+  padding: 10px 0;
+}
+
+.header .logo img{
+  height: 50px;
+}
+
+.tal{
+  float: left;
+  width: 244px;
+  height: 100px;
+  line-height: 100px;
+}
+
+.tal div:first-child{
+  display: inline-block;
+  margin-right: 10px;
+  color: #8d9095;
+}
+
+.tal div:first-child img{
+  position: relative;
+  top: -3px;
+}
+
+.tal div:last-child{
+  display: inline-block;
+  width: 86px; 
+  height: 31px;
+  text-align: center;
+  border-radius: 5px;
+  line-height: 31px;
+  background-color: rgb(58, 121, 232);
+  color: rgb(255, 255, 255);
+}
+
+.footer{
+  width: 100%;
+  height: 82px;
+  line-height: 82px;
+  text-align: center;
+  color: #8d9095;
+  position: relative;
+  background-color: #262626;
+}
+
+.footer div {
+  position: absolute;
+  width: 34px;
+  height: 34px;
+  top: 24px;
+  right: 12px;
+  background-color: red;
+}
+
+</style>
---
+++ select/src/views/Home.vue
@@ -0,0 +1,338 @@
+<template>
+  <div class="home">
+    <div class="lunbo">
+      <img src="../../public/img/lunbo.jpg" alt="">
+    </div>
+    <div class="con">
+      <div class="con_l">
+        <ul class="clear">
+          <li>
+            <img src="../../public/img/con_1.jpg" alt="">
+          </li>
+          <li>
+            <img src="../../public/img/con_2.jpg" alt="">
+          </li>
+          <li>
+            <img src="../../public/img/con_3.jpg" alt="">
+          </li>
+          <li>
+            <img src="../../public/img/con_4.jpg" alt="">
+          </li>
+        </ul>
+        <div class="main">
+          <div class="a">
+            <div class="a_tit clear">
+              <h2>
+                  全新楼盘                  
+              </h2>
+              <h3>
+                  - New Apartment -                  
+              </h3>
+            </div>
+            <ul class="a_con clear">
+              <li>
+                <img src="../../public/img/aaa.jpg" alt="">
+                <div>
+                    <span class="name">
+                        W3322-10088号楼盘
+                    </span><br>
+                    <span>地区：W3322-10088</span><br>
+                    <span>户型：32m² - 64m²</span><br>
+                    <span>价格：21000元/m²</span>
+                </div>
+              </li>
+              <li>
+                <img src="../../public/img/aaa.jpg" alt="">
+                <div>
+                    <span class="name">
+                        W3322-10088号楼盘
+                    </span><br>
+                    <span>地区：W3322-10088</span><br>
+                    <span>户型：32m² - 64m²</span><br>
+                    <span>价格：21000元/m²</span>
+                </div>
+              </li>
+              <li>
+                <img src="../../public/img/aaa.jpg" alt="">
+                <div>
+                    <span class="name">
+                        W3322-10088号楼盘
+                    </span><br>
+                    <span>地区：W3322-10088</span><br>
+                    <span>户型：32m² - 64m²</span><br>
+                    <span>价格：21000元/m²</span>
+                </div>
+              </li>
+            </ul>
+            <div class="duo">
+              <div>浏览更多新楼盘</div>
+            </div>
+          </div>
+          <div class="a">
+              <div class="a_tit clear">
+                <h2>
+                    全新楼盘                  
+                </h2>
+                <h3>
+                    - New Apartment -                  
+                </h3>
+              </div>
+              <ul class="a_con clear">
+                <li>
+                  <img src="../../public/img/aaa.jpg" alt="">
+                  <div>
+                      <span class="name">
+                          W3322-10088号楼盘
+                      </span><br>
+                      <span>地区：W3322-10088</span><br>
+                      <span>户型：32m² - 64m²</span><br>
+                      <span>价格：21000元/m²</span>
+                  </div>
+                </li>
+                <li>
+                  <img src="../../public/img/aaa.jpg" alt="">
+                  <div>
+                      <span class="name">
+                          W3322-10088号楼盘
+                      </span><br>
+                      <span>地区：W3322-10088</span><br>
+                      <span>户型：32m² - 64m²</span><br>
+                      <span>价格：21000元/m²</span>
+                  </div>
+                </li>
+                <li>
+                  <img src="../../public/img/aaa.jpg" alt="">
+                  <div>
+                      <span class="name">
+                          W3322-10088号楼盘
+                      </span><br>
+                      <span>地区：W3322-10088</span><br>
+                      <span>户型：32m² - 64m²</span><br>
+                      <span>价格：21000元/m²</span>
+                  </div>
+                </li>
+              </ul>
+              <div class="duo">
+                <div>浏览更多新楼盘</div>
+              </div>
+            </div>
+            <div class="a">
+                <div class="a_tit clear">
+                  <h2>
+                      全新楼盘                  
+                  </h2>
+                  <h3>
+                      - New Apartment -                  
+                  </h3>
+                </div>
+                <ul class="a_con clear">
+                  <li>
+                    <img src="../../public/img/aaa.jpg" alt="">
+                    <div>
+                        <span class="name">
+                            W3322-10088号楼盘
+                        </span><br>
+                        <span>地区：W3322-10088</span><br>
+                        <span>户型：32m² - 64m²</span><br>
+                        <span>价格：21000元/m²</span>
+                    </div>
+                  </li>
+                  <li>
+                    <img src="../../public/img/aaa.jpg" alt="">
+                    <div>
+                        <span class="name">
+                            W3322-10088号楼盘
+                        </span><br>
+                        <span>地区：W3322-10088</span><br>
+                        <span>户型：32m² - 64m²</span><br>
+                        <span>价格：21000元/m²</span>
+                    </div>
+                  </li>
+                  <li>
+                    <img src="../../public/img/aaa.jpg" alt="">
+                    <div>
+                        <span class="name">
+                            W3322-10088号楼盘
+                        </span><br>
+                        <span>地区：W3322-10088</span><br>
+                        <span>户型：32m² - 64m²</span><br>
+                        <span>价格：21000元/m²</span>
+                    </div>
+                  </li>
+                </ul>
+                <div class="duo">
+                  <div>浏览更多新楼盘</div>
+                </div>
+              </div>
+
+        </div>
+      </div>
+      <div class="con_r">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  .clear:after{
+    display:block;
+    clear:both;
+    content:"";
+    visibility:hidden;
+    height:0
+  } 
+
+  .clear{
+    zoom:1
+  }
+
+  .lunbo{
+    width: 1257px;
+    height: 600px;
+    margin: 0 auto;
+    overflow: hidden;
+    margin-bottom: 118px;
+  }
+
+  .lunbo img{
+    margin-left: -271.5px;
+  }
+
+  .con{
+    width: 1200px;
+    margin: 0 auto;
+    padding: 0 40px;
+  }
+
+  .con > div.con_l{
+    width: 820px;
+  }
+
+  .con > div.con_l > ul{
+    margin-bottom: 40px;
+  }
+
+  .con > div.con_l > ul li{
+    float: left;
+    margin: 5px;
+  }
+
+  .con > div.con_l > ul li:nth-of-type(1){
+    margin-left: 0;
+    margin-top: 0;
+  }
+
+  .con > div.con_l > ul li:nth-of-type(2){
+    margin-right: 0;
+    margin-top: 0;
+  }
+  
+  .con > div.con_l > ul li:nth-of-type(3){
+    margin-left: 0;
+    margin-bottom: 0;
+  }
+
+  .con > div.con_l > ul li:nth-of-type(4){
+    margin-right: 0;
+    margin-bottom: 0;
+  }
+
+  .con > div.con_l > ul li > img{
+    width: 405px;
+  }
+
+  .main{
+    margin-right: 15px;
+  }
+
+  .a{
+    width: 818px;
+    height: 500px;
+    margin-top: 40px;
+    border: 1px solid #e2e2e2;
+    background-color: #F7F7F7;
+  }
+  .a_tit{
+    height: 72px;
+    text-align: center;
+  }
+
+  .a_tit h2{
+    margin: 25px 0 5px 5px;
+    line-height: 29px;
+    font-size: 22px;
+    font-family: '微软雅黑';
+    font-weight: bold;
+  }
+  .a_tit h3{
+    margin: 0 0 22px 0;
+    line-height: 14px;
+    font-size: 16px;
+    font-family: 'arial';
+    color: #aaaaaa;
+  }
+
+  .a_con li{
+    float: left;
+    width: 270px;
+    height: 310px;
+    margin-right: 2px;
+    margin-bottom: 20px;
+    background-color: #fff;
+  }
+
+  .a_con li img{
+    width: 270px;
+  }
+
+  .a_con li div{
+    padding-left: 15px;
+  }
+
+  .a_con li div .name{
+    display: inline-block;
+    font-family: '微软雅黑';
+    margin: 15px 0;
+    font-weight: bold;
+    padding: 5px 0 8px 0;
+    margin-bottom: 5px;
+  }
+
+  .a_con li div span{
+    margin-top: -15px;
+    line-height: 22px;
+  }
+
+  .a .duo{
+    height: 64px;
+    width: 100%;
+    position: relative;
+  } 
+
+  .a .duo div{
+    width: 134px;
+    height: 36px;
+    line-height: 36px;
+    text-align: center;
+    border: 1px solid #3a79e8;
+    position: absolute;
+    left: 50%;
+    margin-left: -67px;
+    transition: all .2s ease-in-out
+  } 
+
+  .a .duo div:hover{
+    border-color: #000000;
+  } 
+
+
+</style>
